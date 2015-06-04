25bccc78ad58242847cb0f54a7b944fd5013df27
Theo Ng <theodore_ng@hotmail.com>
2015-06-04 02:05:02 -0700

    tailer+failed attempt at mailer

--- src/servicemonitor/MyMailer.java
+++ src/servicemonitor/MyMailer.java
@@ -1,5 +1,51 @@
 package servicemonitor;
 
-public class MyMailer {
+import java.util.Date;
+import java.util.Properties;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+import java.util.Properties;
+ 
+public class MyMailer {
+	
+	public static void main(String[] args) {
+		 
+		final String username = "crazyokuni@gmail.com";
+		final String password = "crazyokuni1991";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("crazyokuni@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("theodore.n.ng@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
